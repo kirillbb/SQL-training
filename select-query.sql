c17f3d8013110e388497e0f3cab9d8c0724e35ae
Kirill_divine <kirill.sokolov96@gmail.com>
2023-03-14 22:14:43 +0200

    added querys to select data

--- select-query.sql
+++ select-query.sql
@@ -5,3 +5,16 @@ SELECT TOP (1000) [Id]
       ,[Phone]
       ,[Salary]
   FROM [userDB].[dbo].[Users]
+
+  SELECT [Id]
+        ,[Name] + ' / ' + [Email]  + ' ( ' + [Phone] + ' )'  as Contact
+        ,[Salary] * 12 AS YearSalary
+    FROM [userDB].[dbo].[Users]
+    ORDER BY [YearSalary] DESC
+
+SELECT DISTINCT [Name]
+    INTO UniqueUsers
+FROM [userDB].[dbo].[Users]
+
+SELECT * 
+    FROM UniqueUsers
